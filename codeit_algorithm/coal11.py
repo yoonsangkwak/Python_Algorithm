fb86b3c560644eb7613e49c7410776953a206307
yoonsangkwak <yunsang08@naver.com>
2020-08-09 11:32:19 +0900

    modified text

--- codeit_algorithm/coal11.py
+++ codeit_algorithm/coal11.py
@@ -24,7 +24,7 @@ def trapping_rain(buildings):
     total_rain = 0
 
     # 리스트의 각 인덱스을 돌면서 해당 칸에 담기는 빗물의 양을 구한다
-    # 0번 인덱스와 마지막 인덱스는 볼 필요 없다
+    # 0번 인덱스와 마지막 인덱스는 볼 필요 없다 (어차피 빗물이 안담기니까)
     for i in range(1, len(buildings) - 1):
         # 현재 인덱스를 기준으로 양쪽에 가장 높은 건물의 위치를 구한다
         max_left = max(buildings[:i])
@@ -34,7 +34,7 @@ def trapping_rain(buildings):
         can_rain = min(max_left, max_right)
 
         # 현재 인덱스에 담기는 빗물의 양을 계산
-        # 만약 upper_bound가 현재 인덱스 건물보다 높지 않다면, 현재 인덱스에 담기는 빗물은 0
+        # 만약 can_rain이 현재 인덱스 건물보다 높지 않다면, 현재 인덱스에 담기는 빗물은 0
         total_rain += max(0, can_rain - buildings[i])
 
     return total_rain
