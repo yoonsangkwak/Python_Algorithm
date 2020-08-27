442be383a546545ca2ca496a2b19c0a6009e4d4f
yoonsangkwak <yunsang08@naver.com>
2020-08-27 17:50:31 +0900

    baek 2748

---
+++ backjoon_python/back71.py
@@ -0,0 +1,16 @@
+# 2748번
+
+n = int(input())
+
+fibonacci = []
+num = 0
+for i in range(n+1):
+    if i == 0:
+        num = 0
+    elif i <= 2:
+        num = 1
+    else:
+        num = fibonacci[-1] + fibonacci[-2]
+    fibonacci.append(num)
+
+print(fibonacci[-1])
