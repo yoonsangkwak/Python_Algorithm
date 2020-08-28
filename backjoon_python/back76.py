311bae7ddb95be69def32b79b75baee8572f8bee
yoonsangkwak <yunsang08@naver.com>
2020-08-28 11:07:12 +0900

    baek 2775

--- backjoon_python/back76.py
+++ backjoon_python/back76.py
@@ -2,16 +2,12 @@
 
 T = int(input())
 
-def home(k, n):
-    if k == 0:
-        return n
-    people = 0
-    for j in range(1, n+1):
-        people += home(k-1, j)
-    return people
-
-
 for i in range(T):
     k = int(input())
     n = int(input())
-    print(home(k, n))
+    people = [i for i in range(1, n+1)]
+
+    for x in range(k):
+        for y in range(1, n):
+            people[y] += people[y-1]
+    print(people[-1])
