d8113b295d152cf893a298df275d15d7a342dfd0
yoonsangkwak <yunsang08@naver.com>
2020-08-29 22:14:42 +0900

    Big-O(n)

---
+++ codeit_algorithm/coal38.py
@@ -0,0 +1,31 @@
+def trapping_rain(buildings):
+    total_height = 0 # 총 갇히는 비의 양을 담을 변수
+    n = len(buildings)
+
+    # 각각 왼쪽 오른쪽 최대값 리스트 정의
+    left_list = [0] * n
+    right_list = [0] * n
+
+    # buildings 리스트 각 인덱스 별로 왼쪽으로의 최댓값을 저장한다
+    left_list[0] = buildings[0]
+    for i in range(1, n):
+        left_list[i] = max(left_list[i - 1], buildings[i])
+                
+    # buildings 리스트 각 인덱스 별로 오른쪽으로의 최댓값을 저장한다
+    right_list[-1] = buildings[-1]
+    for i in range(n - 2, -1, -1):
+        right_list[i] = max(right_list[i + 1], buildings[i])
+
+    # 저장한 값들을 이용해서 총 갇히는 비의 양을 계산한다
+    for i in range(n):
+        # 현재 인덱스에 빗물이 담길 수 있는 높이
+        upper_bound = min(right_list[i], left_list[i])
+
+        # 현재 인덱스에 담기는 빗물의 양을 계산
+        # 만약 upper_bound가 현재 인덱스 건물보다 높지 않다면, 현재 인덱스에 담기는 빗물은 0
+        total_height += max(0, upper_bound - buildings[i])
+
+    return total_height
+
+print(trapping_rain([3, 0, 0, 2, 0, 4]))
+print(trapping_rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
