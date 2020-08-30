917df6e7706d80f24f0170b5a3a0feee7dee93b2
yoonsangkwak <yunsang08@naver.com>
2020-08-30 15:22:52 +0900

    baek 1920

---
+++ backjoon_python/back77.py
@@ -0,0 +1,28 @@
+# 1920번
+
+def binary_search(element, some_list, start=0, end=None):
+    if end == None:
+        end = len(some_list) - 1
+    if start > end:
+        return 0
+        
+    mid = (start + end) // 2
+    
+    if element == some_list[mid]:
+        return 1
+    elif element < some_list[mid]:
+        end = mid - 1
+    elif element > some_list[mid]:
+        start = mid + 1
+    return binary_search(element, some_list, start, end)
+
+
+n = int(input())
+n_list = list(map(int, input().split()))
+sorted_list = sorted(n_list)
+
+m = int(input())
+m_list = list(map(int, input().split()))
+
+for i in range(len(m_list)):
+    print(binary_search(m_list[i], sorted_list))
