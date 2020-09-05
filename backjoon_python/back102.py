ede00893a71a2fc4f0cba397719d7c433155196e
yoonsangkwak <yunsang08@naver.com>
2020-09-05 15:17:02 +0900

    baek 1002

--- backjoon_python/back102.py
+++ backjoon_python/back102.py
@@ -12,16 +12,14 @@ for _ in range(T):
 
     if x1 == x2 and y1 == y2:
         if r1 == r2:
-            location = -1
+            print(-1)
         else:
-            location = 0
-        continue
+            print(0)
     
-    if r1 > distance + r2 or r2 > distance + r1 or distance > r1 + r2:
-        location = 0
-    elif r1 == distance + r2 or r2 == distance + r1 or r1 + r2 == distance:
-        location = 1
     else:
-        location = 2
-
-    print(location)
+        if r1 > distance + r2 or r2 > distance + r1 or distance > r1 + r2:
+            print(0)
+        elif r1 == distance + r2 or r2 == distance + r1 or r1 + r2 == distance:
+            print(1)
+        else:
+            print(2)
