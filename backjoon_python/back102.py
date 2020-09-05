cf861ce939824983df0db794f815dff8169534e0
yoonsangkwak <yunsang08@naver.com>
2020-09-05 15:54:25 +0900

    baek 1002

--- backjoon_python/back102.py
+++ backjoon_python/back102.py
@@ -7,7 +7,6 @@ T = int(input())
 
 for _ in range(T):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    location = 0
     distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
     if x1 == x2 and y1 == y2:
