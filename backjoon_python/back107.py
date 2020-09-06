77a6d10edb33c5c7eb6b7692af07f46b43cf8bce
yoonsangkwak <yunsang08@naver.com>
2020-09-06 15:25:43 +0900

    baek 1094

---
+++ backjoon_python/back107.py
@@ -0,0 +1,17 @@
+# 1094번
+# https://yoonsang-it.tistory.com/
+
+x = int(input())
+
+stick = [64, 32, 16, 8, 4, 2, 1]
+count = 0
+
+for i in range(len(stick)):
+    if x >= stick[i]:
+        count += 1
+        x -= stick[i]
+
+    if x == 0:
+        break
+
+print(count)
