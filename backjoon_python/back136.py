3a0d982c5d6c3d609ee1b2a7f4d29d48aa459c5c
yoonsangkwak <yunsang08@naver.com>
2020-09-15 10:41:53 +0900

    baek 10815

---
+++ backjoon_python/back136.py
@@ -0,0 +1,55 @@
+# 10815번
+# https://yoonsang-it.tistory.com/
+
+import sys
+
+n = int(sys.stdin.readline())
+n_list = list(map(int, sys.stdin.readline().split()))
+
+m = int(sys.stdin.readline())
+m_list = list(map(int, sys.stdin.readline().split()))
+
+dict1 = {m_list[i]: 0 for i in range(len(m_list))}
+
+for i in range(n):
+    if n_list[i] in dict1.keys():
+        dict1[n_list[i]] += 1
+print(" ".join(map(str, list(dict1.values()))))
+
+
+"""
+def binary_search(element, some_list, start=0, end=None):
+    if end == None:
+        end = len(some_list) - 1
+    if start > end:
+        return 0
+
+    mid = (start + end) // 2
+
+    if element == some_list[mid]:
+        return 1
+    elif element < some_list[mid]:
+        end = mid - 1
+    elif element > some_list[mid]:
+        start = mid + 1
+    
+    return binary_search(element, some_list, start, end)
+
+import sys
+
+n = int(sys.stdin.readline())
+n_list = list(map(int, sys.stdin.readline().split()))
+
+m = int(sys.stdin.readline())
+m_list = list(map(int, sys.stdin.readline().split()))
+
+sorted_list = sorted(n_list)
+
+answer_list = []
+
+for i in range(len(m_list)):
+    answer_list.append(binary_search(m_list[i], sorted_list))
+
+answer = " ".join(map(str, answer_list))
+print(answer)
+"""
