275b21fbcc3833dd028c2ba75710d07bdbfff3b7
yoonsangkwak <yunsang08@naver.com>
2020-09-26 11:02:51 +0900

    baek 1120

---
+++ backjoon_python/back146.py
@@ -0,0 +1,14 @@
+# 1120번
+# https://yoonsang-it.tistory.com/
+
+a, b = input().split()
+
+answer = []
+for i in range(len(b) - len(a) + 1):
+    count = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            count += 1
+    answer.append(count)
+
+print(min(answer))
